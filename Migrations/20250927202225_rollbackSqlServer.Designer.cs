0127cd467fb84a1c2212f5dac6d0d31e07e9770d
SergioBuzi <sergio.buzi96@gmail.com>
2025-09-27 22:23:07 +0200

    database aligned

--- Migrations/20250927202225_rollbackSqlServer.Designer.cs
+++ Migrations/20250927202225_rollbackSqlServer.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ccalendar.Data;
 
@@ -11,9 +12,11 @@ using ccalendar.Data;
 namespace ccalendar.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250927202225_rollbackSqlServer")]
+    partial class rollbackSqlServer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250927202225_rollbackSqlServer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ccalendar.Migrations
+{
+    /// <inheritdoc />
+    public partial class rollbackSqlServer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastVisitDate",
+                table: "Customers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Customers",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Customers",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastVisitDate",
+                table: "Customers",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
